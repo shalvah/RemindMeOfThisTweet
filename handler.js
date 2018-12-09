9caac414c4ff4b742855be5bfb616d8c57378b6e
shalvah <diakon.ng@gmail.com>
2018-12-09 08:27:33 +0100

    Complete handlers

--- handler.js
+++ handler.js
@@ -28,17 +28,19 @@ module.exports.fetchTweetsAndSetReminders = async (event, context, callback) => 
     let results = allMentions.map(service.parseReminderTime);
     await results.map(service.handleParsingResult);
 
-    finish(callback, cache).success(`Published ${count} tweets`);
+    finish(callback, cache).success(`Handled ${count} tweets`);
 };
 
 module.exports.remind = async (event, context, callback) => {
     const cache = await makeCache();
     const twitter = makeTwitter(cache);
+    const service = makeService(cache);
 
-    const time = event.reminderTime;
-    if (parseInt(event.reminderTime) - (new Date).getTime() < 100) {
+    const tweet = event.data;
+    await Promise.all([
+        twitter.replyWithReminder(tweet),
+        service.cleanup(event.ruleName),
+    ]);
 
-    }
-
-    finish(callback, cache).success(`Published ${count} tweets`);
+    finish(callback, cache).success(`Reminded for tweet: ${JSON.stringify(tweet)}`);
 };
