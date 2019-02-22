51e643d16099f979d70adab80b69ff3a3b14530c
shalvah <diakon.ng@gmail.com>
2019-02-22 12:46:16 +0100

    Webhooks: add scripts for configuration

---
+++ bin/create_webhook.js
@@ -0,0 +1,25 @@
+const request = require('request-promise');
+const args = process.argv.splice(2);
+
+require('dotenv').config('../.env');
+
+const options = {
+    url: 'https://api.twitter.com/1.1/account_activity/all/' + args[0] + '/webhooks.json',
+    oauth: {
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        token: process.env.TWITTER_ACCESS_TOKEN,
+        token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    },
+    headers: {
+        'Content-type': 'application/x-www-form-urlencoded'
+    },
+    form: {
+        url: args[1]
+    }
+};
+
+
+request.post(options)
+    .then(console.log)
+    .catch(console.log);
