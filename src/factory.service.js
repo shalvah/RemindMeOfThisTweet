49f79015721b5088e3ed9a193de5a43631aad0ba
shalvah <diakon.ng@gmail.com>
2019-07-15 21:38:45 +0100

    Bugfix: avoid the null trap.

--- src/factory.service.js
+++ src/factory.service.js
@@ -34,13 +34,14 @@ const make = (cache, twitter) => {
 
     const cancelReminder = async (tweet) => {
         const cacheKey = tweet.referencing_tweet + '-' + tweet.author;
+        console.log(`CANCEL ${cacheKey}`);
         let reminderDetails = await cache.getAsync(cacheKey);
         if (reminderDetails) {
             reminderDetails = JSON.parse(reminderDetails);
             const remindersOnThatDate = await cache.lrangeAsync(reminderDetails.date, 0, -1);
             const indexOfTheReminderWeWant = remindersOnThatDate.map(JSON.parse)
                 .findIndex(r => (r.author == tweet.author) && (reminderDetails.original_tweet == r.id));
-            if (indexOfTheReminderWeWant) {
+            if (indexOfTheReminderWeWant != null) {
                 await Promise.all([
                     cache.delAsync(cacheKey),
                     // Atomic way to remove element from Redis list by index
