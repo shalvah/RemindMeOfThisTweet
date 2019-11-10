34629c0500a323e87bc7cceac787d6974b43f949
shalvah <diakon.ng@gmail.com>
2019-11-10 17:56:45 +0100

    Add support for sending notifications via Firebase

---
+++ src/notifications.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const firebase = require('firebase-admin');
+const serviceAccount = require("../remindmeofthistweet-serviceaccount.json");
+firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+    projectId: process.env.FIREBASE_PROJECT_ID,
+
+});
+
+module.exports = {
+    async sendNotification(username, url) {
+        const token = settings.fbToken;
+        const message = {
+            data: {
+                title: `Here's your reminder, @${username}.👋`,
+                url,
+            },
+            token
+        };
+
+        return firebase.messaging().send(message)
+            .then((response) => {
+                console.log('notification.send.success:' + JSON.stringify(response));
+            })
+            .catch((error) => {
+                console.log('notification.send.error:' + JSON.stringify(error));
+            });
+    }
+};
